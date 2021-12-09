51cf218cb7e9200f646d27d8f2b1a10c3f49172c
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-09 16:55:17 -0700

    Update Instructions.js

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -3,7 +3,7 @@ import React from "react";
 const Instructions = () => {
   return (
     <div className="instructions">
-      <h1 style={{ color: "Crimson" }}>About Skynet</h1>
+      <h1 style={{ color: "#16151D" }}>About Skynet</h1>
       <h3 style={{ color: "white" }}>
         Skynet sets Limits for all of the Admin based Audit Events that can be triggered.
         If a user reaches or exceeds the limits the bot will remove all their roles as long as it has the needed permissions.
@@ -17,18 +17,18 @@ const Instructions = () => {
       <h3 style={{ color: "white" }}>
         • <a href="https://discordapp.com/api/oauth2/authorize?client_id=650872568374493185&permissions=8&scope=bot">Invite</a>
       </h3>
-      <h1 style={{ color: "Crimson" }}>Commands</h1>
+      <h1 style={{ color: "#16151D" }}>Commands</h1>
       <h3 style={{ color: "white" }}>
-      <code style={{ color: "Crimson" }}>sky.help</code> - Displays the Help Message
+      <code style={{ color: "#16151D" }}>sky.help</code> - Displays the Help Message
       </h3>
       <h3 style={{ color: "white" }}>
-      <code style={{ color: "Crimson" }}>sky.limits</code> - Displays the Current Limits set for your Guild.
+      <code style={{ color: "#16151D" }}>sky.limits</code> - Displays the Current Limits set for your Guild.
       </h3>
       <h3 style={{ color: "white" }}>
-      <code style={{ color: "Crimson" }}>sky.recent</code> - Displays a List of Recent Events provide a User ID for Events relating to that User.
+      <code style={{ color: "#16151D" }}>sky.recent</code> - Displays a List of Recent Events provide a User ID for Events relating to that User.
       </h3>
       <h3 style={{ color: "white" }}>
-      <code style={{ color: "Crimson" }}>sky.reset</code> - Reset the Limits to Default Value provide no args for Help.
+      <code style={{ color: "#16151D" }}>sky.reset</code> - Reset the Limits to Default Value provide no args for Help.
       </h3>
     </div>
   );
