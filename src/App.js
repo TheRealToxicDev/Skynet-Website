0b81d0f32b52f4b43dd9ba82465fa333fe12d1c4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-09 16:57:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Toxic Dev
+           &nbsp;Toxic Dev
         </a>
       </div>
     </div>
